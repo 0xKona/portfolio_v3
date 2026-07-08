51091419a52c1be39690b8607f140c37d8c9665d
0xKona <konarobinson@outlook.com>
2026-07-08 09:45:43 +0100

    chore: add stage tag to all resources

--- packages/backend/lib/tags.ts
+++ packages/backend/lib/tags.ts
@@ -1,6 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 
 export function applyTags(scope: cdk.App): void {
+  const stage = scope.node.tryGetContext("stage") ?? "test";
   cdk.Tags.of(scope).add("Application", "portfolio-v3");
   cdk.Tags.of(scope).add("ManagedBy", "CDK");
+  cdk.Tags.of(scope).add("Stage", stage);
 }
